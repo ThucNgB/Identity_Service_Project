ce705d1110ce9fa9133a076c40abc6062b843e0d
Thuc Ngoc Bui <thucngb.dev@gmail.com>
2024-08-09 18:14:43 +0700

    initial project

---
+++ src/test/java/thucngocb/identity_service_project/IdentityServiceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package thucngocb.identity_service_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityServiceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
